26bf55016b34a2e15ccce26128e4a91ae4222ed6
Nguyễn Bá Tùng <tungrs94@gmail.com>
2020-08-07 09:42:25 +0700

    update some

--- blog/settings.py
+++ blog/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,4 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'homepage/home.html')
+
+
+def thongtin(request):
+    return render(request, 'homepage/thongtin.html')
